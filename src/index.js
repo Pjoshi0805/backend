44c13c2efd871fe7b4dd3faba50191bb55865dc3
Paras Joshi <paras521851@gmail.com>
2026-02-19 21:19:34 +0530

    setup project file

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import dotenv from 'dotenv'
+import mongoose from "mongoose";
+import {DB_NAME} from "./constants.js"
+
+
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./env'
+})
+
+
+connectDB()
+
+
+
+
+
+// import express from "express";
+// const app = express();
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);   
+//     console.log("Connected to MongoDB");
+//     app.on("error", (error) => {
+//       console.error("Error in Express app:", error);
+//       throw error; // Rethrow the error to be caught by the outer catch block
+//     })
+
+//     app.listen(process.env.PORT, () => {
+//       console.log(`Server is running on port ${process.env.PORT}`);
+//     });
+//   } catch (error) {
+//     console.error("Error connecting to MongoDB:", error);
+//   } 
+// })();
