b0dc33b155db4b45b89743a1f6f8427de5efa625
Paras Joshi <paras521851@gmail.com>
2026-02-21 18:51:37 +0530

    Api responses

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv'
 import mongoose from "mongoose";
 import {DB_NAME} from "./constants.js"
 
-
+import { app } from './app.js';
 import connectDB from "./db/index.js";
 
 dotenv.config({
@@ -11,6 +11,20 @@ dotenv.config({
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000 , ()=>{
+        console.log(`Server is running at port :${process.env.PORT}`)
+    })
+    app.on("error",(error)=>{
+          console.log("ERROR OCCURED IN EXPRESS !!!")
+          throw error
+    })
+})
+.catch((error)=>{
+    console.log("MONGO DB CONNNECTION FAILED !!!",error)
+})
+
+
 
 
 
